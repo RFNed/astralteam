74daa1a8ef33f95b907c7384b3033e8b811749ca
RFNed <166419840+RFNed@users.noreply.github.com>
2026-06-15 07:13:04 +0000

    fixed __init__.py, and added to config.py website url

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,10 +1,13 @@
 from config import config
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI(docs_url=("/docs" if config.is_debug == "True" else None))
-app.add_middleware(CORSMiddleware, allow_origins=['http://127.0.0.6:5173'])
+app.add_middleware(CORSMiddleware, allow_origins=[f'{config.frontend_url}'])
 
 
 @app.get("/socials")
 async def socials_get():
     return {"github": config.github}
+
--- config.py
+++ config.py
@@ -1,10 +1,11 @@
-from load_dotenv import load_dotenv
+from dotenv import load_dotenv
 from os import getenv
 
 load_dotenv()
 
 class Config:
     def __init__(self):
+        self.frontend_url = getenv("WEBSITE_URL")
         self.is_debug = getenv("IS_DEBUG", "False")
         self.github = getenv("GITHUB_LINK", "https://github.com")
 
