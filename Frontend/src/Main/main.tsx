5219cb67e16f23573c3dacf86b16a1ebb9e3f80c
RFNed <maytvey@gmail.com>
2026-07-27 20:19:45 +0300

    first commit!

---
+++ Frontend/src/Main/main.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export default function Main() {
+    return (
+        <>
+        
+        <Link to="catalogue">Astral Team</Link>
+        
+        </>
+    )
+}
---
+++ Frontend/src/core.tsx
@@ -0,0 +1,44 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Link, Route, Routes, useLocation } from 'react-router-dom'
+import './index.css'
+import Main from './Main/main'
+
+function Head() {
+  return (
+    <>
+    
+    <div className='head-bar'>
+      <Link to="/" style={{"textDecoration": "none"}}><img src="/logo.png"/></Link>
+      <div className='catalogue'>
+        <span>КАТАЛОГ</span>
+        <span>НОВОСТИ</span>
+        <span>О НАС</span>
+        <span>WIKI</span>
+      </div>
+      <div className='user-circle'>
+        Регистрация
+      </div>
+    </div>
+    
+    </>
+  )
+}
+
+function Pages() {
+  const location = useLocation()
+  return (
+    <Routes location={location}>
+      <Route path="/" element={<Main />} />
+    </Routes>
+  )
+}
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Head />
+      <Pages />
+    </BrowserRouter>
+  </StrictMode>,
+)
