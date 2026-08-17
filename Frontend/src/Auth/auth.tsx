dc7d9efebb26cb96c900bd90855cb28a47532a7d
RFNed <maytvey@gmail.com>
2026-08-17 23:32:08 +0300

    inited auth

--- Frontend/src/Auth/auth.tsx
+++ Frontend/src/Auth/auth.tsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import "./auth.css"
 
 export default function Auth() {
+    const [login, setLogin] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const handleLogin = async () => {
+        if (login.trim() === "" || password.trim() === "") {
+            alert("Пожалуйста, заполните все поля.");
+            return;
+        }
+    };
+
     return (
         <>
         
         <div className="auth">
-            123
+            <div className="auth-input">
+                <input type="text" placeholder="Логин" maxLength={36} value={login} onChange={(e) => setLogin(e.target.value)} />
+                <input type="password" placeholder="Пароль" maxLength={100} value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <button onClick={handleLogin}>Войти</button>
+            </div>
         </div>
         
         </>
