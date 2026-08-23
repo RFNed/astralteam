c66d01d4d1feae6b460376c168f9475643acc08f
RFNed <maytvey@gmail.com>
2026-08-23 17:55:47 +0300

    auth update updated: notifybox, added states for notfiybox(error/notify)

--- Frontend/src/Pages/Auth/auth.tsx
+++ Frontend/src/Pages/Auth/auth.tsx
@@ -5,11 +5,21 @@ export default function Auth() {
     const [login, setLogin] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
+    const [NotifyBoxText, setNotifyBoxText] = useState<string>("");
+    const [VisibleBoxText, setVisibleBoxText] = useState<boolean>(false);
+    const [IsNotifyError, setIsNotifyError] = useState<boolean>(false); 
+
     const handleLogin = async () => {
         if (login.trim() === "" || password.trim() === "") {
-            alert("Пожалуйста, заполните все поля.");
-            return;
+            setNotifyBoxText("Введите все поля")
+            setVisibleBoxText(true)
+            setIsNotifyError(false)
+            return
         }
+
+        setNotifyBoxText("Неверный логин или пароль")
+        setVisibleBoxText(true)
+        setIsNotifyError(true)
     };
 
     return (
@@ -17,20 +27,28 @@ export default function Auth() {
         
         <div className="auth">
             <div className="auth-input">
-                
+                <div className={`auth-info ${VisibleBoxText ? "visible" : ""}`}  style={{
+                        "backgroundColor": IsNotifyError ? "rgba(228, 13, 13, 0.712)" : "rgba(238, 147, 11, 0.71)",
+                        "filter": IsNotifyError ? "drop-shadow(0px 0px 20px rgb(238, 10, 10))" : "drop-shadow(0px 0px 20px rgb(255, 187, 0))"
+                    }}>
+                    {NotifyBoxText}
+                </div>
                 <span>Авторизация</span>
-                <input type="text" placeholder="Логин" maxLength={36} value={login} onChange={(e) => setLogin(e.target.value)} />
-                <input type="password" placeholder="Пароль" maxLength={100} value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <input type="text" placeholder="Логин" maxLength={36} value={login} onChange={(e) => {setLogin(e.target.value); setVisibleBoxText(false)}} />
+                <input type="password" placeholder="Пароль" maxLength={100} value={password} onChange={(e) => {setPassword(e.target.value); setVisibleBoxText(false)}}/>
                 <button onClick={handleLogin}>Войти</button>
             </div>
+
             <div className="reg-input">
                 <span>Нет аккаунта?</span>
                 <button>Зарегистрироваться</button>
             </div>
+
             <div className="auth-right-page">
                 <img src="/pics/astralcat.png" />
-                <span>Со входом — больше возможностей! 😼</span>
+                <span>Со входом — больше возможностей!</span>
             </div>
+
         </div>
         
         </>
